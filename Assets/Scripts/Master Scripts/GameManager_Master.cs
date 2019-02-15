ca5b1bbb2728998e5289330b17d37a55fdb18bb5
ozymandias <onuratasaritas@gmail.com>
2019-02-15 19:21:16 +0300

    Created event system and game manager

---
+++ Assets/Scripts/Master Scripts/GameManager_Master.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MuseumC {
+	public class GameManager_Master : MonoBehaviour
+{
+	public delegate void GameManagerEventHandler();
+	public event GameManagerEventHandler MenuToggleEvent;
+	public event GameManagerEventHandler InventoryUIToggleEvent;
+	public event GameManagerEventHandler RestartLevelEvent;
+	public event GameManagerEventHandler GoToMenuSceneEvent;
+	public event GameManagerEventHandler GameOverEvent;
+	
+	public bool isGameOver;
+	public bool isInventoryUIOn;
+	public bool isMenuOn;
+	
+	
+    
+    public void CallEventMenuToggle(){
+		if (MenuToggleEvent != null) {
+			MenuToggleEvent();
+		}
+	}
+	
+	public void CallEventInventoryUIToggle(){
+		if (InventoryUIToggleEvent != null) {
+			InventoryUIToggleEvent();
+		}
+	}
+	
+	public void CallEventRestartLevelToggle(){
+		if (RestartLevelEvent != null) {
+			RestartLevelEvent();
+		}
+	}
+	
+	public void CallEventGoToMenuSceneToggle(){
+		if (GoToMenuSceneEvent != null) {
+			GoToMenuSceneEvent();
+		}
+	}
+	
+	public void CallEventGameOverToggle(){
+		if (GameOverEvent != null) {
+			isGameOver = true;
+			GameOverEvent();
+		}
+	}
+}
+	
+}
