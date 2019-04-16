fb7ce33522b43d65f4af1b992aaab7c9485a4c1e
saygindikbayir <hdikbayir@gmail.com>
2019-04-16 18:55:13 +0300

    Location Detection of Girl

--- Assets/NavNeshWalk.cs
+++ Assets/NavNeshWalk.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class NavNeshWalk : MonoBehaviour
+public class LittleGirlWalk : MonoBehaviour
 {
     public Transform Destination;
+    public Transform Destination2;
+    public Transform Destination3;
+    public Transform Destination4;
 
     private NavMeshAgent _agent;
-    private Animator _anim;
+    //private Animator _anim;
     // Start is called before the first frame update
     void OnEnable()
     {
         _agent = GetComponent<NavMeshAgent>();
-        _anim = GetComponent<Animator>();
-        _anim.Play("Standing Idle");
+        //_anim = GetComponent<Animator>();
+        //_anim.Play("Standing Idle");
         StartCoroutine(WalkInSeconds(0.4f));
     }
 
@@ -28,28 +31,37 @@ public class NavNeshWalk : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            WalkToDestination();
-        }
+
 
         if (!_agent.pathPending)
         {
             if (_agent.pathStatus == NavMeshPathStatus.PathComplete)
             {
-                if (Vector3.Distance(Destination.position, transform.position) <= 0.6f)
+                if (Vector3.Distance(Destination2.position, transform.position) <= 2.0f)
                 {
-                    Debug.Log("arrived");
-                    _anim.Play("Standing Idle");
+                    _agent.destination = Destination3.position;
+                }
+                if (Vector3.Distance(Destination.position, transform.position) <= 2.0f)
+                {
+                    Debug.Log("arrived at destination 1");
+                    //_anim.Play("Standing Idle");
+                    _agent.destination = Destination2.position;
+
+                }
+                if (Vector3.Distance(Destination3.position, transform.position) <= 2.0f)
+                {
+                    _agent.destination = Destination4.position;
                 }
             }
         }
     }
 
+
     public void WalkToDestination()
     {
         _agent.destination = Destination.position;
-        _anim.Play("Walking 1");
+        //_anim.Play("Walking 1");
         Debug.Log("go");
     }
+
 }
