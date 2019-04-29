07d7cdc69bccf309062fc705cb4bb1945669d132
saygindikbayir <hdikbayir@gmail.com>
2019-04-30 00:37:27 +0300

    Çay Ninesi JS

---
+++ Assets/NehirJS.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NehirJS : MonoBehaviour
+{
+    private Animator anim;
+    public GameObject trigger;
+
+    private void OnEnable()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private IEnumerator WalkInSeconds(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        PlayJumpScare();
+        yield break;
+    }
+
+    private void PlayJumpScare()
+    {
+        anim.Play("Zombie Neck Bite");
+    }
+ 
+}
