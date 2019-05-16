71e2e60ea3c6c3330f7d2ae6814f7764f01253d7
avicend <utku.d.m@gmail.com>
2019-05-16 16:10:57 +0300

    Alkarısı Animations and Navmesh with Scene Transitions

---
+++ Assets/Alkarısı/alkarisiFollow.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class alkarisiFollow : MonoBehaviour
+{
+    private NavMeshAgent alkarisi;
+    private Animator animAlkarisi;
+    public GameObject target;
+
+    public void goToDestination() {
+
+        
+
+        alkarisi.destination = target.transform.position;
+        
+    }
+
+    private void OnEnable()
+    {
+        alkarisi = GetComponent<NavMeshAgent>();
+        animAlkarisi = GetComponent<Animator>();
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        alkarisi.stoppingDistance = 2.3f;
+
+            
+            goToDestination();
+
+
+        if (alkarisi.remainingDistance<=2.3f)
+        {
+            animAlkarisi.SetBool("Catched", true);
+        }
+
+        else
+        {
+            animAlkarisi.SetBool("Catched", false);
+            StartCoroutine(LoadScene());
+            
+
+        }
+         
+        
+
+        
+        
+
+    }
+
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(15.0f);
+        SceneManager.UnloadScene("BurningVillage 1");
+            SceneManager.LoadScene("Karacong", LoadSceneMode.Single);
+    }
+}
